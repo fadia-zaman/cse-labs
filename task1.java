8639a8f06453ac71f73b3bf963e5bbc4ff9e89e4
Fadia Zaman <fadiazaman04@gmail.com>
2026-01-18 00:55:44 +0600

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,17 @@
+public class task1{
+  public static void main ( String[]  args){
+    int a=100;
+    int b=23;
+    int c= -4;
+    if (a>b && a>c){
+      System.out.println("largest number:"+a);
+    }
+    else if (b>a && b>c){
+      System.out.println("largest number:"+b);
+    }
+    else {
+      System.out.println("largest number:"+c);
+    }
+  }
+}
+  
